d2e25f5568ea04534e76f042ff67c76606ca121f
Pram Baharsyah <mbaharsyah@gmail.com>
2013-10-15 22:26:59 +0800

    External Toggle Demo

--- EditableTextViewApp/src/com/kimikanen/editabletextviewtest/MainActivity.java
+++ EditableTextViewApp/src/com/kimikanen/editabletextviewtest/MainActivity.java
@@ -2,6 +2,10 @@ package com.kimikanen.editabletextviewtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kimikanen.views.EditableTextView;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final EditableTextView etv5 = (EditableTextView) findViewById(R.id.etv5);
+		final Button btnToggle = (Button) findViewById(R.id.btn5);
+
+		btnToggle.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				etv5.setEditMode(!etv5.isEditMode());
+			}
+		});
 	}
 
 }
