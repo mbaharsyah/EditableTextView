60f89ce63ffc2d4e147855a48d241ac3966c9679
Pram Baharsyah <mbaharsyah@gmail.com>
2013-10-20 22:59:15 +0800

    Bugfix: avoid onTouch ACTION_DOWN event to be passed to parent when clicking on the drawable

--- EdittableTextViewLib/src/com/kimikanen/views/EditableTextView.java
+++ EdittableTextViewLib/src/com/kimikanen/views/EditableTextView.java
@@ -71,7 +71,6 @@ public class EditableTextView extends EditText {
 		this.editMode = isEditMode;
 
 		refreshState();
-		requestFocus();
 		requestLayout();
 		invalidate();
 	}
@@ -91,45 +90,62 @@ public class EditableTextView extends EditText {
 			setCompoundDrawablesWithIntrinsicBounds(null, null,
 					editMode ? saveDrawable : editDrawable, null);
 		}
+		if (editMode) {
+			requestFocus();
+		} else {
+			clearFocus();
+		}
+	}
+
+	private boolean isDrawableClicked(int actionX, int actionY) {
+		boolean retval = false;
+		if (showButton) {
+			final Drawable drawableRight = isEditMode() ? saveDrawable
+					: editDrawable;
+			final Rect bounds = drawableRight.getBounds();
+
+			int x, y;
+			final int extraTapArea = 10;
+
+			x = actionX + extraTapArea;
+			y = actionY - extraTapArea;
+
+			x = getWidth() - x;
+			if (x <= 0) {
+				x += extraTapArea;
+			}
+
+			if (y <= 0) {
+				y = actionY;
+			}
+			retval = bounds.contains(x, y);
+		}
+
+		return retval;
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		Rect bounds;
-		if (event.getAction() == MotionEvent.ACTION_UP) {
-			int actionX = (int) event.getX();
-			int actionY = (int) event.getY();
-			if (showButton) {
-				final Drawable drawableRight = isEditMode() ? saveDrawable
-						: editDrawable;
-				bounds = drawableRight.getBounds();
-
-				int x, y;
-				final int extraTapArea = 10;
-
-				x = actionX + extraTapArea;
-				y = actionY - extraTapArea;
-
-				x = getWidth() - x;
-				if (x <= 0) {
-					x += extraTapArea;
-				}
-
-				if (y <= 0) {
-					y = actionY;
-				}
-
-				if (bounds.contains(x, y)) {
-					setEditMode(!editMode);
-					event.setAction(MotionEvent.ACTION_CANCEL);
-
-					if (onEditModeChangedListener != null) {
-						onEditModeChangedListener.onEditModeChanged(this,
-								editMode);
-					}
-					return true;
-				}
+		final int actionX = (int) event.getX();
+		final int actionY = (int) event.getY();
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			if (isDrawableClicked(actionX, actionY)) {
+				// return true here to avoid parent processing the event
+				return true;
 			}
+			break;
+		case MotionEvent.ACTION_UP:
+			if (isDrawableClicked(actionX, actionY)) {
+				setEditMode(!editMode);
+
+				if (onEditModeChangedListener != null) {
+					onEditModeChangedListener.onEditModeChanged(this, editMode);
+				}
+				event.setAction(MotionEvent.ACTION_CANCEL);
+				return true;
+			}
+			break;
 		}
 		return super.onTouchEvent(event);
 	}
